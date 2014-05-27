01d125f2bc2789bc833662b21959e0c86999288d
Jan Ekström <jan.ekstrm@intel.com>
2014-05-27 15:59:43 +0300

    tests: Remove the SWIG stuff, will not be used (most probably)

--- tests/swig/build_mappings.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-swig -v -Wall -Wextra -python -I../.. mrp_resource_api.i
-
-gcc -Wall -Wextra -shared -fPIC -o _mrp_resource_api.so -I/usr/include/python2.7 -I../.. -I../../src/plugins/resource-native/libmurphy-resource mrp_resource_api_wrap.c -L../../src/.libs -lmurphy-resource -lmurphy-common
