35b65a93f383a4699bb8181de875c823e1c29538
Jan Ekström <jan.ekstrm@intel.com>
2014-06-06 14:40:58 +0300

    tests: Remove test_ from the resource_native_helpers' file name

--- tests/resource-native_example.py
+++ tests/resource-native_example.py
@@ -29,7 +29,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from mrp_resource_native import (Connection)
-from test_mrp_resource_native_helpers import StateDump
+from mrp_resource_native_helpers import py_res_callback
 import sys
 
 
@@ -59,55 +59,6 @@ def py_status_callback(conn, error_code, opaque):
                 print("\tAttribute: %s = %s" % (attr_name, attr.get_value()))
 
     print('ResCtxCallback ErrCode: %d' % (error_code))
-
-
-def py_res_callback(new_res_set, opaque):
-    print("ResCallBack: Entered")
-
-    # Get the old res set from the opaque data
-    res_set = opaque.res_set
-
-    # Check if this callback is for the resource set we have in userdata
-    if not new_res_set:
-        print("ResCallBack: No resource set yet set")
-        return
-    elif not res_set.equals(new_res_set):
-        print("ResCallBack: Callback not for carried resource set")
-        return
-
-    dump = StateDump(res_set)
-    dump.state = "acquired"
-    dump.resources["audio_playback"].state = "acquired"
-    dump.resources["audio_playback"].attributes[res_set.get_resource_by_name("audio_playback"). \
-                                                list_attribute_names()[0]].value = "huehue"
-
-    new_dump = StateDump(new_res_set)
-    print("ResCallBack: Did my StateDump work? %s" % dump.equals(new_dump))
-
-    # Print information about the old resource set state
-    print("ResCallBack: Previously resource set was: %s" %
-          (res_set.get_state()))
-
-    # Print information about the new resource set state
-    print("ResCallBack: Resource set now is: %s" %
-          (new_res_set.get_state()))
-
-    # Compare the resources, and check which ones have changed
-    for resource in new_res_set.list_resource_names():
-        old_resource     = res_set.get_resource_by_name(resource)
-        checked_resource = new_res_set.get_resource_by_name(resource)
-
-        if old_resource.get_state() != checked_resource.get_state():
-            attr = checked_resource.get_attribute_by_name(checked_resource.list_attribute_names()[0])
-            print("ResCallBack: The status of resource '%s' has changed: %s -> %s" %
-                  (resource, old_resource.get_state(),
-                   checked_resource.get_state()))
-            print("ResCallBack: Attribute %s = %s" % (attr.get_name(), attr.get_value()))
-
-    # Remove and switch the userdata resource set to a new one
-    res_set.update(new_res_set)
-
-    opaque.res_set_changed = True
 
 
 def actual_test_steps(conn):
