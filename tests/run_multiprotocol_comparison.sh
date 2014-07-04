eb6b8507a8383f657099d43a1a9702f5e1f4c5f6
Jan Ekström <jan.ekstrom@intel.com>
2014-07-04 14:17:38 +0300

    build/tests: Initial implementation of the multiprotocol test
    Does not yet compare values, but will error out if the result
    in a specific test is different.

--- tests/run_multiprotocol_comparison.sh
+++ tests/run_multiprotocol_comparison.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+TEST_CONFS=( "test_mrp_dbus.conf" "test_mrp_resource.conf" )
 ACTIONS=`fmbt -o adapter=dummy test_mrp_dbus.conf 2>/dev/null | fmbt-log -f '$ax'`
-fmbt -i test_mrp_dbus.conf <<< "${ACTIONS}"
-fmbt -i test_mrp_resource.conf <<< "${ACTIONS}"
-# printf '%s' "${ACTIONS}" | fmbt -i test_mrp_dbus.conf
 
-echo ${PIPESTATUS[@]}
+run_test()
+{
+test_name=${1}
+    fmbt -i "${test_name}" <<< "${ACTIONS}" 2> "${test_name%.*}_test_run.log"
+}
+
+for i in "${TEST_CONFS[@]}"
+do
+    run_test "$i"
+done
+
+diff -u *_test_run.log
+exit $?
