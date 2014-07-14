2f7abdd3b965ec7d560549f32ff7f27dd6cbabf9
Jan Ekström <jan.ekstrom@intel.com>
2014-07-14 16:27:59 +0300

    tests/dbus: Move an example callback to dbus helpers and use it
    This updates the D-Bus example as well as the tests to the new way
    of defining changes.

--- tests/mrp_dbus_helpers.py
+++ tests/mrp_dbus_helpers.py
@@ -29,6 +29,25 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+def example_callback(prop, value, original_thing, config, user_data):
+    print(">> PythonicCallback")
+
+    # Basic per-callback debug log for property and new value
+    print("PythonicCallback[%s]: %s = %s" % (str(original_thing), prop, value))
+
+    # Using the ChangeManager to handle incoming changes
+    if user_data.was_this_an_expected_change(original_thing, prop, value):
+        print("PythonicCallback: This change was expected!")
+    else:
+        print("PythonicCallback: This change was not expected")
+
+    # When we are no longer expecting new changes, we stop the mainloop
+    if not user_data.changes_available():
+        config.reset_mainloop()
+
+    print("<< PythonicCallback")
+
+
 # Things we can actually add, such as resources or sets
 class Addition(object):
     def __init__(self, name, added_path):
@@ -172,72 +191,3 @@ class ChangeManager():
                     return True
 
         return False
-
-
-class StateDumpResource(object):
-    def __init__(self, res):
-        self.name = res.get_name()
-        self.state = res.get_state()
-        self.attr_names  = []
-        self.attr_values = []
-
-        for name in res.list_attribute_names():
-            self.attr_names.append(name)
-            self.attr_values.append(res.get_attribute_value(name))
-
-        self.attributes = dict(zip(self.attr_names, self.attr_values))
-
-    def equals(self, other):
-        # If we have a different amount of attributes we definitely aren't dealing with the same thing
-        if len(other.attr_names) != len(self.attr_names):
-            return False
-
-        # If one of the values is not the same, we stop at that and return False
-        for name in self.attr_names:
-            if self.attributes[name] != other.attributes[name]:
-                return False
-
-        # And finally there's the state
-        return self.state == other.state
-
-    def print_differences(self, other):
-        print("\tResource %s:" % (self.name))
-        if self.state != other.state:
-            print("\t\tState: %s != %s" % (self.state, other.state))
-
-        for name in self.attr_names:
-            if self.attributes[name] != other.attributes[name]:
-                print("\t\tAttribute %s: %s != %s" % (name, self.attributes[name], other.attributes[name]))
-
-
-class StateDump(object):
-    def __init__(self, res_set):
-        self.names       = []
-        self.res_objects = []
-        self.state = res_set.get_state()
-
-        for name in res_set.list_resource_names():
-            self.names.append(name)
-            self.res_objects.append(StateDumpResource(res_set.get_resource_by_name(name)))
-
-        self.resources = dict(zip(self.names, self.res_objects))
-
-    def equals(self, other):
-        for res in self.res_objects:
-            if not res.equals(other.resources[res.name]):
-                return False
-
-        return self.state == other.state
-
-    def print_differences(self, other):
-        print("Resource Set:")
-        if self.state != other.state:
-            print("\tState: %s != %s" % (self.state, other.state))
-
-        for res in self.res_objects:
-            res.print_differences(other.resources[res.name])
-
-    def set_acquired(self):
-        self.state = "acquired"
-        for res in self.resources.itervalues():
-            res.state = "acquired"
