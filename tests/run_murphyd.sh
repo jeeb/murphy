dc4ee4e4bd487de0f5dd0dfbcef47ea1e740493b
Jan Ekström <jan.ekstrom@intel.com>
2014-07-17 11:51:45 +0300

    tests: Add the Valgrind opts as a comment to murphy startup script

--- tests/run_murphyd.sh
+++ tests/run_murphyd.sh
@@ -6,6 +6,7 @@ if pidof "lt-murphyd" >/dev/null; then
 else
     echo "Murphy is not yet running, starting..."
     ../src/murphyd -P ../src/.libs -c conf/murphy-lua.conf -vv &
+    # -V --leak-check=full --track-origins=yes --trace-children=yes
     # Murphy does not yet release a signal that it has finished starting
     # and thus we sleep
     sleep 1
