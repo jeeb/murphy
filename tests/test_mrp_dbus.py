dbc23a70a14ddfd762ee446da819812cec019451
Jan Ekström <jan.ekstrom@intel.com>
2014-07-17 09:55:26 +0300

    tests/dbus: Major overhaul of the D-Bus testing model and adapters
    Now the D-Bus testing takes ~190 steps to complete.

--- tests/test_mrp_dbus.py
+++ tests/test_mrp_dbus.py
@@ -39,9 +39,6 @@ config.set_bus_type("session")
 conn = None
 res_sets = []
 resources = []
-conn_callback_set = False
-res_set_callback_set = False
-resource_callback_set = False
 
 c_manager = ChangeManager()
 
@@ -57,13 +54,13 @@ def value_to_be_set(type):
 
 def connect():
     print(">>> Connect")
-    global config, conn, conn_callback_set
+    global config, conn
+
     conn = Connection(config)
     print(conn.pretty_print())
     assert conn
-    if not conn_callback_set:
-        conn.register_callback(example_callback, c_manager)
-        conn_callback_set = True
+
+    conn.register_callback(example_callback, c_manager)
     print("<<< Connect")
 
 
@@ -73,151 +70,196 @@ def disconnect():
     for res_set in res_sets:
         assert res_set.delete()
         res_sets.remove(res_set)
+
     print(conn.pretty_print())
+    conn.get_mainloop().quit()
+    conn = None
+
     print("<<< Disconnect")
 
 
 def create_res_set():
     print(">>> CreateResSet")
-    global conn, res_sets, res_set_callback_set
+    global conn, res_sets
+
     res_set = conn.create_resource_set()
     assert res_set
-    if not res_set_callback_set:
-        res_set.register_callback(example_callback, c_manager)
-        res_set_callback_set = True
 
+    res_set.register_callback(example_callback, c_manager)
     c_manager.add_change(conn, ResSetAddition(res_set.get_path()))
+
     conn.get_mainloop().run()
     res_sets.append(res_set)
+
     print(conn.pretty_print())
     print("<<< CreateResSet")
 
 
 def remove_res_set():
     print(">>> RemoveResSet")
-    global conn, res_sets, res_set_callback_set
+    global conn, res_sets
+
     res_set = res_sets.pop()
+
     c_manager.remove_changes(res_set)
     c_manager.add_change(conn, ResSetRemoval(res_set.get_path()))
     assert res_set.delete()
-    res_set_callback_set = False
+
     conn.get_mainloop().run()
+
     print(conn.pretty_print())
     print("<<< RemoveResSet")
 
 
-def set_class():
+def set_class(failure_expected=False):
     print(">>> SetClass")
+
     res_set = res_sets[0]
-    assert res_set.set_class("navigator")
-    c_manager.add_change(res_set, ClassModification("navigator"))
+
+    if failure_expected:
+        assert not res_set.set_class("navigator")
+    else:
+        assert res_set.set_class("navigator")
+        c_manager.add_change(res_set, ClassModification("navigator"))
+        conn.get_mainloop().run()
+        assert res_set.get_class() == "navigator"
+
     print(res_set.pretty_print())
-    conn.get_mainloop().run()
-    assert res_set.get_class() == "navigator"
     print("<<< SetClass")
 
 
 def add_resource():
     print(">>> AddResource")
-    global res_sets, resource_callback_set
+    global res_sets
+
     res_set = res_sets[0]
     res = res_set.add_resource(res_set.list_available_resources()[0])
     assert res
-    if not resource_callback_set:
-        res.register_callback(example_callback, c_manager)
-        resource_callback_set = True
+    res.register_callback(example_callback, c_manager)
     c_manager.add_change(res_set, ResourceAddition(res.get_path()))
-    r_list = res_set.list_resources()
-    print(r_list)
+
     conn.get_mainloop().run()
+
     print(res_set.pretty_print())
     print("<<< AddResource")
 
 
 def remove_resource():
     print(">>> RemoveResource")
-    global res_sets, resource_callback_set
+    global res_sets
+
     res_set = res_sets[0]
     res_name = res_set.list_resources()[0]
     res = res_set.get_resource(res_name)
     assert res
+
     c_manager.remove_changes(res)
     c_manager.add_change(res_set, ResourceRemoval(res.get_path()))
     assert res_set.remove_resource(res)
+
     conn.get_mainloop().run()
-    resource_callback_set = False
     assert res_name not in res_set.list_resources()
+
     print(res_set.pretty_print())
     print("<<< RemoveResource")
 
 
-def modify_attribute():
+def modify_attribute(failure_expected=False):
     print(">>> ModifyAttribute")
     global res_sets
+
     res_set = res_sets[0]
     res = res_set.get_resource(sample(res_set.list_resources(), 1)[0])
     assert res
 
     attr_name = res.list_attribute_names()[0]
     assert attr_name
-
     attr_type = res.get_attribute_type(attr_name)
+
     print("ModifyAttribute: Setting attribute %s to value %s" % (attr_name, value_to_be_set(attr_type)))
 
-    assert res.set_attribute_value(attr_name, value_to_be_set(attr_type))
-    c_manager.add_change(res, AttributeModification(attr_name, value_to_be_set(attr_type)))
-    conn.get_mainloop().run()
+    if failure_expected:
+        assert not res.set_attribute_value(attr_name, value_to_be_set(attr_type))
+    else:
+        assert res.set_attribute_value(attr_name, value_to_be_set(attr_type))
+        c_manager.add_change(res, AttributeModification(attr_name, value_to_be_set(attr_type)))
+        conn.get_mainloop().run()
+        assert res.get_attribute_value(attr_name) == value_to_be_set(attr_type)
+
     print(res.pretty_print())
-    assert res.get_attribute_value(attr_name) == value_to_be_set(attr_type)
     print("<<< ModifyAttribute")
 
 
-def make_resource_mandatory():
+def make_resource_mandatory(failure_expected=False):
     print(">>> MakeMandatory")
+
     res_set = res_sets[0]
     res = res_set.get_resource(res_set.list_resources()[0])
-    res.make_mandatory()
-    c_manager.add_change(res, MandatorynessModification(True))
-    conn.get_mainloop().run()
+
+    if failure_expected:
+        assert not res.make_mandatory()
+    else:
+        assert res.make_mandatory()
+        c_manager.add_change(res, MandatorynessModification(True))
+        conn.get_mainloop().run()
+        assert res.is_mandatory()
+
     print(res.pretty_print())
-    assert res.is_mandatory()
     print("<<< MakeMandatory")
 
 
-def make_resource_nonessential():
+def make_resource_nonessential(failure_expected=False):
     print(">>> MakeNonessential")
+
     res_set = res_sets[0]
     res = res_set.get_resource(res_set.list_resources()[0])
-    res.make_mandatory(False)
-    c_manager.add_change(res, MandatorynessModification(False))
-    conn.get_mainloop().run()
+
+    if failure_expected:
+        assert not res.make_mandatory(False)
+    else:
+        assert res.make_mandatory(False)
+        c_manager.add_change(res, MandatorynessModification(False))
+        conn.get_mainloop().run()
+        assert not res.is_mandatory()
+
     print(res.pretty_print())
-    assert not res.is_mandatory()
     print("<<< MakeNonessential")
     pass
 
 
-def make_resource_shareable():
+def make_resource_shareable(failure_expected=False):
     print(">>> MakeShareable")
+
     res_set = res_sets[0]
     res = res_set.get_resource(res_set.list_resources()[0])
-    res.make_shareable()
-    c_manager.add_change(res, SharingModification(True))
-    conn.get_mainloop().run()
+
+    if failure_expected:
+        assert not res.make_shareable()
+    else:
+        assert res.make_shareable()
+        c_manager.add_change(res, SharingModification(True))
+        conn.get_mainloop().run()
+        assert res.is_shareable()
+
     print(res.pretty_print())
-    assert res.is_shareable()
     print("<<< MakeShareable")
 
 
-def make_resource_unshareable():
+def make_resource_unshareable(failure_expected=False):
     print(">>> MakeUnshareable")
+
     res_set = res_sets[0]
     res = res_set.get_resource(res_set.list_resources()[0])
-    res.make_shareable(False)
-    c_manager.add_change(res, SharingModification(False))
-    conn.get_mainloop().run()
+
+    if failure_expected:
+        assert not res.make_shareable(False)
+    else:
+        assert res.make_shareable(False)
+        c_manager.add_change(res, SharingModification(False))
+        conn.get_mainloop().run()
+        assert not res.is_shareable()
+
     print(res.pretty_print())
-    assert not res.is_shareable()
     print(">>> MakeUnshareable")
     pass
 
@@ -225,22 +267,30 @@ def make_resource_unshareable():
 def acquire_set():
     print(">>> AcquireSet")
     global res_sets
+
     res_set = res_sets[0]
+
     assert res_set.request()
     c_manager.add_change(res_set, Acquisition())
     conn.get_mainloop().run()
-    print(res_set.pretty_print())
     assert res_set.get_state() == "acquired"
+
+    print(res_set.pretty_print())
     print("<<< AcquireSet")
 
 
 def release_set():
     print(">>> ReleaseSet")
     global res_sets
+
     res_set = res_sets[0]
+    will_we_get_callback = not (res_set.get_state() != "acquired")
+
     assert res_set.release()
-    c_manager.add_change(res_set, Release())
-    conn.get_mainloop().run()
+    if will_we_get_callback:
+        c_manager.add_change(res_set, Release())
+        conn.get_mainloop().run()
+        assert res_set.get_state() == "available"
+
     print(res_set.pretty_print())
-    assert res_set.get_state() == "available"
     print("<<< ReleaseSet")
