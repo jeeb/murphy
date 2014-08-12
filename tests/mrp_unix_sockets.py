2e5a3c2cff82e77a167fde1ff4e36bd8c5386ac9
Jan Ekström <jan.ekstrom@intel.com>
2014-08-12 15:32:00 +0300

    tests/unix_sockets: Un-duplicate request code path

--- tests/mrp_unix_sockets.py
+++ tests/mrp_unix_sockets.py
@@ -510,9 +510,9 @@ class MurphyConnection(asyncore.dispatcher_with_send):
 
         return write_uint32(len(byte_stream)) + byte_stream
 
-    def list_resources(self):
+    def send_request(self, request_type):
         status = Status()
-        byte_stream = self.create_request(RESPROTO_QUERY_RESOURCES)
+        byte_stream = self.create_request(request_type)
 
         message = parse_message(byte_stream)
 
@@ -537,57 +537,12 @@ class MurphyConnection(asyncore.dispatcher_with_send):
         del(status)
 
         return response
+
+    def list_resources(self):
+        return self.send_request(RESPROTO_QUERY_RESOURCES)
 
     def list_classes(self):
-        status = Status()
-        byte_stream = self.create_request(RESPROTO_QUERY_CLASSES)
-
-        message = parse_message(byte_stream)
-        self.add_to_queue(message.req_type, message.seq_num, status)
-
-        self.send(byte_stream)
-
-        gatekeeper = status.wait(5.0)
-        self.remove_from_queue(message.req_type, message.seq_num)
-
-        # If gatekeeper tells us that we didn't get a response, we timed out
-        if not gatekeeper:
-            print("E: Timed out on the response (waited five seconds; seq %s - type %s)" % (message.seq_num,
-                                                                                            message.req_type))
-            return None
-
-        # Get the response data from the status object
-        response = status.get_result()
-        print("D: Response gotten:\n%s" % (response.pretty_print()))
-
-        # Delete the status object itself
-        del(status)
-
-        return response
+        return self.send_request(RESPROTO_QUERY_CLASSES)
 
     def list_zones(self):
-        status = Status()
-        byte_stream = self.create_request(RESPROTO_QUERY_ZONES)
-
-        message = parse_message(byte_stream)
-        self.add_to_queue(message.req_type, message.seq_num, status)
-
-        self.send(byte_stream)
-
-        gatekeeper = status.wait(5.0)
-        self.remove_from_queue(message.req_type, message.seq_num)
-
-        # If gatekeeper tells us that we didn't get a response, we timed out
-        if not gatekeeper:
-            print("E: Timed out on the response (waited five seconds; seq %s - type %s)" % (message.seq_num,
-                                                                                            message.req_type))
-            return None
-
-        # Get the response data from the status object
-        response = status.get_result()
-        print("D: Response gotten:\n%s" % (response.pretty_print()))
-
-        # Delete the status object itself
-        del(status)
-
-        return response
+        return self.send_request(RESPROTO_QUERY_ZONES)
