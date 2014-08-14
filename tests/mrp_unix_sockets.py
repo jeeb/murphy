6df5be738fef7db32b231e60c51d48783a30d171
Jan Ekström <jan.ekstrom@intel.com>
2014-08-14 13:47:19 +0300

    tests/unix_sockets: Don't make strings unicode on py2

--- tests/mrp_unix_sockets.py
+++ tests/mrp_unix_sockets.py
@@ -28,7 +28,6 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-from __future__ import unicode_literals
 from threading import Thread
 from socket import (AF_UNIX, AF_INET, AF_INET6, SOCK_STREAM)
 from struct import (calcsize, pack, unpack)
